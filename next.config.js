b105cbdba21a4fc71b67484003c660fffacf1b29
jaylimbachiya43 <109168679+jaylimbachiya43@users.noreply.github.com>
2023-10-16 14:47:31 +0530

    svg works

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    webpack(config, { isServer }) {
+      // For the client
+      if (!isServer) {
+        config.module.rules.push({
+          test: /\.svg$/,
+          issuer: /\.[jt]sx?$/,
+          use: ['@svgr/webpack'],
+        });
+      }
+  
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
