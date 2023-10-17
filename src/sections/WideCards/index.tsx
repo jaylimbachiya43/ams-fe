9e66aec3b697711913908bb2efa55e041ed26980
Jay Parmar <190770107115@socet.edu.in>
2023-10-17 11:47:52 +0530

    sidebar fix

--- src/sections/WideCards/index.tsx
+++ src/sections/WideCards/index.tsx
@@ -1,29 +1,37 @@
-import AltStripes from '@/components/AltStripes/index'
-import DataCard from '@/components/DataCard/index'
-import Holidays from '@/components/Holidays/index'
-import Show from '@/components/Show/index'
-import React from 'react'
-import style from './style.module.css'
+    import AltStripes from '@/components/AltStripes/index'
+    import DataCard from '@/components/DataCard/index'
+    import Holidays from '@/components/Holidays/index'
+    import Show from '@/components/Show/index'
+    import React from 'react'
+    import style from './style.module.css'
 
-const data = [
-    {
-        title: 'Attendance',
-        contentComponent: Show,
-    },
-    {
-        title: 'Personal Details',
-        contentComponent: AltStripes,
-    },
-]
+    const data = [
+        {
+            title: 'Attendance',
+            contentComponent: Show,
+        },
+        {
+            title: 'Personal Details',
+            contentComponent: AltStripes,
+        },
+        {
+            title: 'ersonal Details',
+            contentComponent: AltStripes,
+        },
+        {
+            title: 'rsonal Details',
+            contentComponent: AltStripes,
+        },
+    ]
 
-const WideCards = () => {
-    return (
-        <div className={style.container}>
-            {data.map((item, index) => (
-                <DataCard key={index} item={item} />
-            ))}
-        </div>
-    )
-}
+    const WideCards = () => {
+        return (
+            <div className={style.container}>
+                {data.map((item, index) => (
+                    <DataCard key={index} item={item} />
+                ))}
+            </div>
+        )
+    }
 
-export default WideCards
+    export default WideCards
